7758b13d8b2c742ad5a0d1f792651e479fa65c99
Wire <wirewraith@gmail.com>
2017-08-06 19:46:13 -0400

    New phrases, phrase memory and a refresh phrase button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,14 +6,36 @@
     'GO FER RIT!',
     'Just dook it.',
     'You can do the thing.',
-    'Never give up.'
+    'Never give up.',
+    'In the sweetness of friendship let there by laughter, and the sharing of pleasure. For in the dew of little things, the heart finds it\'s morning and is refreshed.',
+    'Wise is the man who looks at ferrets, for he knows there is no greater joy in life.',
+    'I love you.',
+    'Be yourself.',
+    'I\'m a ferret!',
   ];
 
   const $dialogText = document.querySelector('#dialog-text');
+  const $refresh = document.querySelector('.refresh');
 
   function pickPhrase() {
-    $dialogText.textContent = phrases[Math.floor(Math.random() * phrases.length)];
+    const lastPhraseIndex = parseInt(localStorage.getItem('lastphrase'));
+    let phraseIndex = lastPhraseIndex;
+
+    //Pick a phrase that isn't the one the user just saw, to keep things fresh
+    while (lastPhraseIndex === phraseIndex) {
+      phraseIndex = Math.floor(Math.random() * phrases.length);
+    }
+
+    $dialogText.textContent = phrases[phraseIndex];
+    localStorage.setItem('lastphrase', phraseIndex);
   }
+
+  $refresh.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    pickPhrase();
+  });
 
   pickPhrase();
 
